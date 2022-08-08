e8b29deb32ffc5957fe4c0c46b1fd972f0b1f2fc
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-08-09 00:18:30 +0200

    Fix some bug in authentication endpoints for jwt

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import Base, engine
+from database import Base, SessionLocal, engine
 from routers.auth import router
 from models.User import User
-from database import get_db, Base, SessionLocal
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
+
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+# in production you can use Settings management
+# from pydantic to get secret key from .env
+class Settings(BaseModel):
+    authjwt_secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+# callback to get your configuration
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+# exception handler for authjwt
+# in production, you can tweak performance using orjson response
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
 
 app.add_middleware(
     CORSMiddleware,
--- routers/auth.py
+++ routers/auth.py
@@ -3,43 +3,62 @@ from sqlalchemy.orm import Session
 from schemas.auth import LoginSchema, RegistrationSchema
 from models.User import User
 from database import get_db
-from oauth2 import create_access_token
+from fastapi_jwt_auth import AuthJWT
+from utils.get_current_user import get_current_user
 import datetime
+
 
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post('/login')
-def login(credentials: LoginSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == credentials.email).first()
+@router.post('/login', status_code=status.HTTP_200_OK)
+def login(credentials: LoginSchema, db: Session = Depends(get_db), Authorize: AuthJWT = Depends()):
     
+    # check if user exists
+    user = db.query(User).filter(User.email == credentials.email).first()
     if not user: 
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"La mail {credentials.email} è errata")
         
+    # check if password is correct
     if user.password != credentials.password:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"La password è errata")
+        
+    # update last login and save
     user.last_login = datetime.datetime.now()
     db.commit()
-    token = create_access_token(data={"id": str(user.id)})
-    return {"access_token": token}
+    
+    # generate new tokens
+    access = Authorize.create_access_token(subject=str(user.id))
+    refresh = Authorize.create_refresh_token(subject=str(user.id))
+    return {"access_token": access, "refresh_token": refresh}
 
 
-@router.post('/registration', response_model=RegistrationSchema)
-def registration(credentials: RegistrationSchema, db: Session = Depends(get_db)):
+
+@router.post('/registration', status_code=status.HTTP_201_CREATED)
+def registration(credentials: RegistrationSchema, db: Session = Depends(get_db), Authorize: AuthJWT = Depends()):
+    
+    # check if user altredy exists
     if db.query(User).filter(User.email == credentials.email).first():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"La mail {credentials.email} è usata da un altro utente")
     
+    # create and save user
     user = User(**credentials.dict())
+    user.last_login = datetime.datetime.now()
     db.add(user)
     db.commit()
     db.refresh(user)
-    return user
+    
+    # generate tokens
+    access = Authorize.create_access_token(subject=str(user.id))
+    refresh = Authorize.create_refresh_token(subject=str(user.id))
+    return {"access_token": access, "refresh_token": refresh}
     
 
 
 @router.post('/logout')
-def logout():
-    pass
+def logout(db: Session = Depends(get_db),
+           user: User = Depends(get_current_user)):
+    print(user)
