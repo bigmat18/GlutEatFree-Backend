cca2a65303837e1b0ca58613da1d5dfe2df4ab61
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-09-04 23:58:30 +0200

    Init testing for paragraph

---
+++ tests/test_paragraph.py
@@ -0,0 +1,57 @@
+import pytest
+
+def test_paragraph_create():
+    pass
+
+def test_paragraph_create_unauth():
+    pass
+
+
+
+def test_paragraph_list():
+    pass
+
+
+
+def test_paragraph_retrieve():
+    pass
+
+
+
+def test_paragraph_update():
+    pass
+
+def test_paragraph_update_unauth():
+    pass
+
+
+
+def test_paragraph_delete():
+    pass
+
+def test_paragraph_delete_unauth():
+    pass
+
+
+
+def test_paragraph_image_update():
+    pass
+
+def test_paragraph_image_update_unauth():
+    pass
+
+
+
+def test_paragraph_image_create():
+    pass
+
+def test_paragraph_image_create_unauth():
+    pass
+
+
+
+def test_paragraph_image_delete():
+    pass
+
+def test_paragraph_image_delete_unauth():
+    pass
